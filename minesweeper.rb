02f3ce103b37181f5d203f10e925f677c332f4df
TulioMagnus <tuliokpmagnus@gmail.com>
2023-04-02 11:15:41 -0300

    feat: finished passing the tests

--- minesweeper.rb
+++ minesweeper.rb
@@ -4,6 +4,7 @@ require 'pry'
 
 class Board
   BOARD_FIELDS = ['|', '+', '-', '*'].freeze
+  HEADER_FIELDS = ['+', '-'].freeze
 
   def self.transform(*args)
     new(*args).transform
@@ -14,34 +15,50 @@ class Board
   end
 
   def transform
-    # TODO: raise exception if any character is not acceptable
-    # TODO: raise exception if board matrix is wrong
+    raise ArgumentError unless valid_structure?
+    raise ArgumentError unless valid_board?
+    raise ArgumentError unless valid_characters?
     process_board
   end
 
   private
 
+  def valid_structure?
+    @board.all? { |a| a.size == @board.first.size }        
+  end
+
+  def valid_board?
+    header = @board[0] + @board[-1] - HEADER_FIELDS
+    body = @board[1..-2].all? { |a| a.first == '|' && a.last == a.first }
+    header.empty? && body
+  end
+
+  def valid_characters?
+    reduced_board = @board.flatten - BOARD_FIELDS - [' ']
+    reduced_board.empty?
+  end
+
   def process_board
     @board.each_with_index do |row, line|
-      row.each_with_index do |field, column|
+      row.each_with_index do |field, col|
         next if BOARD_FIELDS.include?(field)
 
-        @board[line][column] = count_bombs(line, column).to_s
+        @board[line][col] = count_bombs(line, col).to_s
       end
     end
     @board.map(&:join)
   end
 
-  def count_bombs(line, column)
+  def count_bombs(line, col)
     bombs_found = 0
     square_area = [
       # LEFT and RIGHT
-      [line - 1, column], [line + 1, column],
+      [line - 1, col], [line + 1, col],
       # ABOVE and BELOW
-      [line, column - 1], [line, column + 1],
+      [line, col - 1], [line, col + 1],
       # ALL 4 DIAGONALS
-      [line - 1, column - 1], [line + 1, column - 1],
-      [line - 1, column + 1], [line + 1, column + 1]
+      [line - 1, col - 1], [line + 1, col - 1],
+      [line - 1, col + 1], [line + 1, col + 1]
     ]
     square_area.each do |square|
       bombs_found += 1 if @board.send(:[], square[0]).send(:[], square[1]) == '*'
