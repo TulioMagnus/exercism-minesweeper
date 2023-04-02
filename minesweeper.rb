df5cdc50918754a512cc7392747ff47e03424839
TulioMagnus <tuliokpmagnus@gmail.com>
2023-04-02 09:12:32 -0300

    feat: created 2d board and filled with zeroes, skipped board chars

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,6 +1,38 @@
-=begin
-Write your code for the 'Minesweeper' exercise in this file. Make the tests in
-`minesweeper_test.rb` pass.
-To get started with TDD, see the `README.md` file in your
-`ruby/minesweeper` directory.
-=end
+# frozen_string_literal: true
+require 'pry'
+
+class Board
+  BOARD_FIELDS = ['|', '+', '-', '*'].freeze
+
+  def self.transform(*args)
+    new(*args).transform
+  end
+
+  def initialize(board)
+    @board = board.map { |str| str.split(//) }
+  end
+
+  def transform
+    # TODO: raise exception if any character is not acceptable
+    # TODO: raise exception if board matrix is wrong
+    process_board
+    binding.pry
+  end
+
+  private
+
+  def process_board
+    @board.each_with_index do |row, i|
+      row.each_with_index do |field, j|
+        next if BOARD_FIELDS.include?(field)
+
+        @board[i][j] = count_bombs(i, j).to_s
+      end
+    end
+    @board
+  end
+
+  def count_bombs(i, j)
+    bombs_in_area = 0
+  end
+end
